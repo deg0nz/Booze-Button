92fed8643b4130308cbbb3eae53bf8996a127cda
Beh <github@beh.wtf>
2022-10-04 23:24:54 +0200

    Use Real ESP pins

--- src/shared.h
+++ src/shared.h
@@ -4,7 +4,7 @@
 #define WIFI_SSID "M+Bs-Booze-Bar-Button"
 #define WIFI_SECRET "need-drinks-faster"
 #define ALIVE_PING_INTERVAL_MS 1000
-#define UDP_LISTEN_PORT 30023
+#define UDP_LISTEN_PORT 2342
 #define BUTTON_DELAY 100
 
 // Signals
@@ -15,11 +15,11 @@
 IPAddress BUTTON_IP(192, 168, 2, 1);
 IPAddress BUTTON_AP_GATEWAY_IP(192, 168, 2, 1);
 IPAddress BUTTON_AP_SUBNET(255, 255, 255, 0);
-#define BUTTON_PIN 2
-#define BUTTON_STATUS_LED_PIN 0
+#define BUTTON_PIN D4
+#define BUTTON_ROTARY_BUTTON_PIN D2
+#define BUTTON_STATUS_LED_PIN D3
 
 // Client/Light settings
-#define LIGHT_LED_PIN 2
+#define LIGHT_LED_PIN D4
 #define LIGHT_NUM_LEDS 1
-#define LIGHT_LED_WIFI_STATUS_PIN 0
 IPAddress LIGHT_IP(192, 168, 2, 2);
